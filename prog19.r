cd94c3de14e1bf92d8283ed3634634c8c01b1f92
𝓛𝓪 𝓢𝓪𝓵𝓿𝓪𝓽𝓸𝓻𝓮 -1864 <bigbadwolf@tutamail.com>
2024-02-03 21:12:49 +0530

    Added prog19.r

---
+++ prog19.r
@@ -0,0 +1,10 @@
+# Initializing sum variable
+sum_result <- 0
+
+# Using a for loop to calculate the sum of the first 10 natural numbers
+for (i in 1:10) {
+  sum_result <- sum_result + i
+}
+
+# Printing the result
+cat("Sum of the first 10 natural numbers is:", sum_result, "\n")
