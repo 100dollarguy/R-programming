2ad114956d14bd9c25fc022e9d1799859ca7d816
𝓛𝓪 𝓢𝓪𝓵𝓿𝓪𝓽𝓸𝓻𝓮 -1864 <bigbadwolf@tutamail.com>
2024-02-03 20:28:32 +0530

    Added prog2

---
+++ prog2.r
@@ -0,0 +1,26 @@
+# Constants
+PI <- 3.14159
+GRAVITY <- 9.81
+
+# Variables
+name <- "John Doe"
+age <- 30
+is_student <- TRUE
+grades <- c(85, 92, 78, 95, 89)
+
+# Displaying information
+cat("Name:", name, "\n")
+cat("Age:", age, "\n")
+cat("Is Student:", is_student, "\n")
+cat("Grades:", grades, "\n")
+
+# Data Types
+ntype <- typeof(name)
+atype <- typeof(age)
+itype <- typeof(is_student)
+gtype <- typeof(grades)
+
+cat("Data Type of 'name':", ntype, "\n")
+cat("Data Type of 'age':", atype, "\n")
+cat("Data Type of 'is_student':", itype, "\n")
+cat("Data Type of 'grades':", gtype, "\n")
