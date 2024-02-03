f6282c205478dbb08c5661e4e77527a40394a757
𝓛𝓪 𝓢𝓪𝓵𝓿𝓪𝓽𝓸𝓻𝓮 -1864 <bigbadwolf@tutamail.com>
2024-02-03 20:06:24 +0530

    Added prog1

---
+++ prog1.r
@@ -0,0 +1,25 @@
+# Creating a Numeric Vector
+n <- c(1, 2, 3, 4, 5)
+print("Numeric Vector:")
+print(n)
+
+# Creating a Character Vector
+cv <- c("apple", "banana", "cherry")
+print("Character Vector:")
+print(cv)
+
+# Creating a List
+lst <- list(1, "apple", TRUE)
+print("List:")
+print(lst)
+
+# Creating a Matrix
+m <- matrix(1:6, nrow = 2, ncol = 3)
+print("Matrix:")
+print(m)
+
+# Creating a Data Frame
+df <- data.frame(Name = c("Alice", "Bob", "Charlie"),
+                 Age = c(25, 30, 22))
+print("Data Frame:")
+print(df)
